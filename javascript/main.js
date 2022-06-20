2b391e32cfaa8ddd46104f3ef776942f252da62a
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-20 18:03:15 +0200

    icons adding

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,22 @@ const drummers = [
       image: ['./images/aric-impronta.jpg'],
       id: 1,
     },
+    {
+    name: 'Tony Royster Jnr',
+    bio: 'Tony Royster Jr. won the 1995 Guitar Center Drum-Off Competition'
+        + 'when he was just 11 years old.',
+    contentBio: 'When he was young, his drumming heroes were two of the greatest fusion drummers in history: Billy Cobham and Dennis Chambers.',
+    image: ['./images/Tony-Royster'],
+    id: 2,
+  },
+  {
+    name: 'John Blackwell Jr.',
+    bio: 'Blackwell began learning the drums when he was three years old from his'
+     + ' father.',
+    contentBio: 'By the time he was 17, he was performing professionally.',
+    image: ['./images/John-blackell.jpg'],
+    id: 3,
+  },
+
+];
+
