b831fc07a619c0075473efb9235416975f487206
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-20 16:31:11 +0200

    Main.js code started and continuing with main.css

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+const drummers = [
+    {
+      name: 'Aric Improta',
+      bio: 'Aric Improta is an important figure in the development of the modern drum set.',
+      contentBio: 'Aric Improta is an explosive live performer, and one of drumming`s true showmen. ',
+      image: ['./images/aric-impronta.jpg'],
+      id: 1,
+    },
