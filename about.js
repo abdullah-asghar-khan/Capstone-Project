cd6ffad2e92cb60137fa4532038ecb16705c0af8
Abdullah2213565 <dulakhan024@gmail.com>
2022-06-22 20:31:24 +0200

    Correcting the about section

--- about.js
+++ about.js
@@ -10,3 +10,34 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+function createPastEventImage({ year, location, image }) {
+  const courosalImageCard = createElement('div', 'courosal-image-card');
+
+  const overlaySlide = createElement('div', 'courosal-image-fade');
+  const label1 = createElement('h3');
+  label1.innerText = year;
+  const label2 = createElement('p');
+  label2.innerText = location;
+  overlaySlide.append(label1, label2);
+
+  const imageEL = createElement('img');
+  imageEL.src = image;
+  imageEL.alt = location;
+
+  courosalImageCard.append(overlaySlide, imageEL);
+  const container = document.querySelector('.past-image-courosal');
+  if (container) {
+    container.appendChild(courosalImageCard);
+  }
+}
+
+function showImages() {
+  pastEventImages.map((album) => createPastEventImage(album));
+}
+showImages();
+
+return {
+  createElement, createCloseIcon, createCard, toggleCard,
+};
+
